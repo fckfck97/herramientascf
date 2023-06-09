e29a05b91ec8d218882b01e58cebe65c1abb7ea5
Jesus Delgado <themurder261@gmail.com>
2023-06-08 20:49:22 -0500

    arreglando css

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -1,10 +1,3 @@
-/**
- * This is a minimal config.
- *
- * If you need the full config, get it from here:
- * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
- */
-
 module.exports = {
     content: [
         // Templates within theme app (e.g. base.html)
